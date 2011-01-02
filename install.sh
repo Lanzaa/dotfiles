98e8eb7802652ac373c802bde0c3237a1830a883
Colin B <lanzaa@gmail.com>
2011-01-01 23:21:35 -0800

    Changed install.sh to be a bit more verbose

--- install.sh
+++ install.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
+
+link () {
+    count=$((count + 1))
+    echo "Linking: $1 -> $2"
+    ln -sf ${source} ${target}
+}
+
+nolink () {
+    echo "Not Linking: $1"
+}
+
+count=$((0))
+
 for i in _*
 do 
     source="${PWD}/$i"
     target="${HOME}/${i/_/.}"
-#     if [ -h "${target}" -o -e "${target}" ]; then
-#         echo "${target} already exists"       
-#     else
-        echo "Linking: ${target} -> ${source}"
-        ln -sf ${source} ${target}
-#    fi
+    if [ -h "${target}" ]; then
+        # Find out where it links to
+        current="$(readlink ${target})"
+        if [ "${current}" == "${source}" ]; then
+            nolink ${target}
+        else
+            link ${target} ${source}
+        fi
+    else
+        link ${target} ${source}
+    fi
 done
+
+echo "Relinked ${count} file(s)."
